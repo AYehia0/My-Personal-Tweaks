c994972beb338c8aea234cf4684aab879b7172b6
AYehia0 <ahmedyehia611@gmail.com>
2022-05-09 22:21:32 +0200

    ranger : doesn't have to start the TelegramClient each time ranger is opened

--- _ranger/commands.py
+++ _ranger/commands.py
@@ -5,12 +5,10 @@ from config import configs
 
 api_id = configs['api_id']
 api_hash = configs['api_hash']
-
-client = TelegramClient("homies", api_id, api_hash)
-client.start()
+session_path = "/path/to/session"
 
 # return the username
-async def get_homie_username(name='ahmed'):
+async def get_homie_username(client, name='ahmed'):
     homies = await client.get_dialogs()
     for homie in homies:
         try:
@@ -19,8 +17,8 @@ async def get_homie_username(name='ahmed'):
         except AttributeError:
             pass
 
-async def send_to_homie(name, file_path):
-    homie_name = await get_homie_username(name)
+async def send_to_homie(client, name, file_path):
+    homie_name = await get_homie_username(client, name)
 
     await client.send_file(homie_name, file_path)
 
@@ -32,7 +30,13 @@ class send_to(Command):
 
         # send the current file to telegram
         # if it's a folder, zip and send
+        try:
+            client = TelegramClient(session_path, api_id, api_hash, request_retries=1, connection_retries=1)
+            client.start()
+        except OSError:
+            return
+            
         current_path = self.fm.thistab.get_selection()[0]
 
-        asyncio.get_event_loop().run_until_complete(send_to_homie(self.arg(1), str(current_path)))
+        asyncio.get_event_loop().run_until_complete(send_to_homie(client, self.arg(1), str(current_path)))
 
